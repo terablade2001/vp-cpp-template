ab9f67151f3a723c8798191382738825e512e1bf
terablade2001 <terablade2001@yahoo.gr>
2020-03-21 12:47:37 +0200

    (0.015): Adding script 'detachProjectFromGit.sh' for detatching the template to independent project.
    This is required in order to use properly the template, by downloading
    all the data from all the required repositories and then make the whole
    project independent of those repositories.
    
    Just clone the repo:
    https://github.com/terablade2001/CCEXP.git
    
    and then from git-supported command window use:
    cd vp-cpp-template/BuildSystems
    ./detachProjectFromGit.sh
    
    This will create a new commit (DETACH) and remove origin for remote,
    thus the whole project will be now independent; user can change anything
    he wants and commit it without messing with the template. If need to link
    it to another bare repository use:
    git remote add origin <..new bare repo path..>.

---
+++ BuildSystems/detachProjectFromGit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd ..
+git submodule init
+git submodule update --remote --recursive
+rm -rf .gitmodules
+rm .git/config
+cp BuildSystems/config .git/config
+git rm -rf --cached ExtLibs
+rm -rf .git/modules/ExtLibs
+cd ExtLibs
+rm -rf .git
+cd ..
+git remote remove origin
+git add --all
+git commit -m "(DETACH):: Detached vp-cpp-template from origin project and libraries."
